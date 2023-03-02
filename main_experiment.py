5c409ad8e6aadbaa29d574ff2c2c81260a7de32b
sitaomin1994 <sitaomin@usc.edu>
2023-03-02 16:10:55 -0500

    change location of config files

--- main_experiment.py
+++ main_experiment.py
@@ -2,7 +2,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="conf", config_name="exp_config_imp_eval")
+@hydra.main(version_base=None, config_path="conf/experiment1/", config_name="conf.yaml")
 def main(cfg: DictConfig) -> None:
 	print(OmegaConf.to_yaml(cfg))
 
